7800293042fab5daca4202adc8e9e1b9ec7beacf
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2014-10-08 17:10:57 +0100

    Branch with the flgen_plate model and other changes

--- lib/resource_tools/association.rb
+++ lib/resource_tools/association.rb
@@ -9,12 +9,12 @@ module ResourceTools::Association
 
     def has_associated(association)
       self.instance_eval %Q{
-        belongs_to :#{association}
+        belongs_to :#{association}, :foreign_key => :id_#{association}_tmp
         attr_accessor :#{association}_id, :#{association}_uuid
 
         before_validation do
-          associate = #{association.to_s.classify}.with_uuid(#{association}_uuid).first unless #{association}_uuid.blank?
-          associate ||= #{association.to_s.classify}.for_lims(id_lims).with_id(#{association}_id).first unless #{association}_id.blank?
+          associate = #{association.to_s.classify}.with_uuid(#{association}_uuid).first! unless #{association}_uuid.blank?
+          associate ||= #{association.to_s.classify}.for_lims(id_lims).with_id(#{association}_id).first! unless #{association}_id.blank?
           self.#{association}= associate unless associate.blank?
         end
 
